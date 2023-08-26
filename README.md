21d4c96799d1b58ee3130c8e98f2fa2170ef4bba
Amitminer <amitkumarsahin@gmail.com>
2023-08-26 13:49:19 +0530

    adding ImageConverter files and Phpstan action..

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # ImageConverter
-convert image to binary:)
+
+A simple PHP-based command-line tool for converting images to binary data and vice versa.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Usage](#usage)
+- [Installation](#installation)
+- [API Documentation](#api-documentation)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+The ImageConverter tool allows you to convert image files to binary data and recover images from binary data. It provides a command-line interface for easy conversion.
+
+## Features
+
+- Convert an image file to binary data.
+- Recover an image from binary data.
+- Supports popular image formats (PNG, JPG, JPEG).
+- Simple and easy-to-use command-line interface.
+
+## Usage
+
+### Prerequisites
+
+- PHP installed on your system.
+- Command-line access.
+
+### Convert an Image to Binary
+
+To convert an image to binary data, use the following command:
+
+```sh
+php ImageConverter.php -c <image_path>
+```
+
+Example:
+
+```sh
+php ImageConverter.php -c myimage.jpg
+```
+
+The binary data will be saved as "myimage.bin" in the same directory.
+
+### Recover an Image from Binary
+
+To recover an image from binary data, use the following command:
+
+```sh
+php ImageConverter.php -r <binary_path>
+```
+
+Example:
+
+```sh
+php ImageConverter.php -r myimage.bin
+```
+
+The image will be saved as "myimage.jpg" in the same directory.
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+```sh
+git clone https://github.com/Amitminer/ImageConverter.git
+```
+
+2. Change directory to the project folder:
+
+```sh
+cd ImageConverter
+```
+
+3. Run the ImageConverter using PHP as described in the [Usage](#usage) section.
+
+## API Documentation
+
+The ImageConverter tool does not have a traditional API but can be accessed via the command-line interface. Please refer to the [Usage](#usage) section for details on how to use it.
+
+## Contributing
+
+Contributions are welcome! If you have ideas for improvements or new features, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+---
---
+++ src/ImageConverter.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Class ImageConverter
+ */
+class ImageConverter {
+    /**
+     * @var array Allowed image formats
+     */
+    private $allowedFormats = ['png', 'jpg', 'jpeg'];
+
+    /**
+     * Convert an image to binary data.
+     *
+     * @param string $imagePath Path to the image file.
+     * @return string Binary image data or error message.
+     */
+    public function imageToBinary($imagePath): ?string {
+        $extension = strtolower(pathinfo($imagePath, PATHINFO_EXTENSION));
+
+        if (!in_array($extension, $this->allowedFormats)) {
+            return "Unsupported image format.";
+        }
+
+        $imageBinary = file_get_contents($imagePath);
+
+        return $imageBinary;
+    }
+
+    /**
+     * Save binary data as an image file.
+     *
+     * @param string $binaryData Binary image data.
+     * @param string $outputPath Path to save the image file.
+     * @return string Success message or error message.
+     */
+    public function saveBinaryAsImage($binaryData, $outputPath): string {
+        if (file_put_contents($outputPath, $binaryData) === false) {
+            return "Failed to save the image.";
+        }
+
+        return "Image converted successfully and saved as: $outputPath";
+    }
+}
+
+if (count($argv) < 3) {
+    echo "Usage: php ImageConverter.php -c <image_path> OR php ImageConverter.php -r <binary_path>\n";
+    exit(1);
+}
+
+$action = $argv[1];
+$filePath = $argv[2];
+
+$converter = new ImageConverter();
+
+if ($action === "-c") {
+    // Convert an image to binary
+    $binaryImage = $converter->imageToBinary($filePath);
+
+    if (strpos($binaryImage, "Error") === false) {
+        $outputPath = pathinfo($filePath, PATHINFO_FILENAME) . ".bin";
+        $result = $converter->saveBinaryAsImage($binaryImage, $outputPath);
+        echo $result . "\n";
+    } else {
+        echo $binaryImage;
+    }
+} elseif ($action === "-r") {
+    // Recover an image from binary
+    $binaryData = file_get_contents($filePath);
+
+    if ($binaryData !== false) {
+        $outputPath = pathinfo($filePath, PATHINFO_FILENAME) . ".jpg";
+        $result = $converter->saveBinaryAsImage($binaryData, $outputPath);
+        echo $result . "\n";
+    } else {
+        echo "Failed to read binary data from file.\n";
+    }
+} else {
+    echo "Invalid action. Use -c to convert an image or -r to recover an image.\n";
+    exit(1);
+}
+?>
